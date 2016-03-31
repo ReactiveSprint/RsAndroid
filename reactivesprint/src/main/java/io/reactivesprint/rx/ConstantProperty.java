6f14c2727bcab1dbe63d0f2d068418a6a3cf765f
Ahmad Baraka <ah-baraka@live.com>
2016-03-31 15:51:48 +0200

    ConstantProperty

---
+++ reactivesprint/src/main/java/io/reactivesprint/rx/ConstantProperty.java
@@ -0,0 +1,29 @@
+package io.reactivesprint.rx;
+
+import rx.Observable;
+
+/**
+ * Created by Ahmad Baraka on 3/30/16.
+ * A property that never changes.
+ * Inspired by [ReactiveCocoa 4](https://github.com/ReactiveCocoa/ReactiveCocoa)
+ */
+public class ConstantProperty<Value> implements PropertyType<Value> {
+    private final Value value;
+
+    /**
+     * Constructs a property with {@code value}
+     */
+    public ConstantProperty(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public Value getValue() {
+        return value;
+    }
+
+    @Override
+    public Observable<Value> getObservable() {
+        return Observable.just(value);
+    }
+}
