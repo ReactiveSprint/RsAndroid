f339ab82a6190d36ac76365cf369d5b448cdf482
Ahmad Baraka <ah-baraka@live.com>
2016-03-31 15:49:39 +0200

    PropertyType

---
+++ reactivesprint/src/main/java/io/reactivesprint/rx/PropertyType.java
@@ -0,0 +1,22 @@
+package io.reactivesprint.rx;
+
+import rx.Observable;
+
+/**
+ * Created by Ahmad Baraka on 3/29/16.
+ * Represents a property that allows observation of its changes.
+ * <p/>
+ * Inspired by [ReactiveCocoa 4](https://github.com/ReactiveCocoa/ReactiveCocoa)
+ */
+public interface PropertyType<Value> {
+    /**
+     * @return Current value of the property.
+     */
+    Value getValue();
+
+    /**
+     * @return An Observable that once subscribed, sends the current value
+     * then will send property's changes over time.
+     */
+    Observable<Value> getObservable();
+}
