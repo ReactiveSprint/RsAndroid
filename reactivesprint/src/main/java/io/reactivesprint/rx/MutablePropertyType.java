8a5cae06d01beeccb180e7b832235acca6ffbca3
Ahmad Baraka <ah-baraka@live.com>
2016-03-31 15:53:41 +0200

    Initial MutablePropertyType

---
+++ reactivesprint/src/main/java/io/reactivesprint/rx/MutablePropertyType.java
@@ -0,0 +1,32 @@
+package io.reactivesprint.rx;
+
+import rx.Observable;
+import rx.Subscription;
+
+/**
+ * Created by Ahmad Baraka on 3/30/16.
+ * Represents an observable property that can be mutated directly.
+ * Inspired by [ReactiveCocoa 4](https://github.com/ReactiveCocoa/ReactiveCocoa)
+ */
+public interface MutablePropertyType<Value> extends PropertyType<Value> {
+    /**
+     * Sets value of the receiver.
+     */
+    void setValue(Value value);
+
+    /**
+     * Binds an {@link Observable} to the receiver, updating the receiver's value to the latest
+     * value sent by the {@code observable.}
+     * <p/>
+     * The binding will automatically terminate when the receiver is finalized,
+     * or when the {@code observable} completes.
+     */
+    Subscription bind(Observable<Value> source);
+
+    /**
+     * Binds to the latest values of {@code source} to the receiver.
+     * <p/>
+     * The binding will automatically terminate when either the receiver or {@code source} is finalized.
+     */
+    Subscription bind(PropertyType<Value> source);
+}
