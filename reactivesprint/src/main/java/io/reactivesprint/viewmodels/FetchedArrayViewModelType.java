7a9de812cd2d6871575f510fafe9aed19b5278c5
Ahmad Baraka <ah-baraka@live.com>
2016-04-03 01:15:39 +0200

    FetchedArrayViewModelType

---
+++ reactivesprint/src/main/java/io/reactivesprint/viewmodels/FetchedArrayViewModelType.java
@@ -0,0 +1,53 @@
+package io.reactivesprint.viewmodels;
+
+import io.reactivesprint.rx.Command;
+import io.reactivesprint.rx.PropertyType;
+import rx.Observable;
+
+/**
+ * Created by Ahmad Baraka on 3/30/16.
+ * ArrayViewModelType which its array is lazily fetched, or even paginated.
+ *
+ * @param <Element>     Type of Elements in Array.
+ * @param <Page>        Type representing page.
+ * @param <FetchInput>  Type of FetchCommand Input.
+ * @param <FetchOutput> Type of FetchCommand Output.
+ */
+public interface FetchedArrayViewModelType<Element extends ViewModelType, Page, FetchInput, FetchOutput>
+        extends ArrayViewModelType<Element> {
+    /**
+     * @return Whether the receiver is refreshing.
+     */
+    PropertyType<Boolean> isRefreshing();
+
+    /**
+     * @return An object representing next page.
+     */
+    Page getNextPage();
+
+    /**
+     * @return Whether the receiver is fetching next page.
+     */
+    PropertyType<Boolean> isFetchingNextPage();
+
+    /**
+     * @return Next Page
+     */
+    PropertyType<Boolean> hasNextPage();
+
+    /**
+     * @return Command which refreshes ViewModels.
+     */
+    Command<FetchInput, FetchOutput> getRefreshCommand();
+
+    /**
+     * @return Command which fetches ViewModels.
+     * If {@code nextPage} is null, this command will refresh, else this command should fetch next page.
+     */
+    Command<FetchInput, FetchOutput> getFetchCommand();
+
+    /**
+     * @return Applies {@link #getFetchCommand()} only if next page is non null or returns {@link Observable#empty()}
+     */
+    Command<FetchInput, FetchOutput> getFetchIfNeededCommand();
+}
