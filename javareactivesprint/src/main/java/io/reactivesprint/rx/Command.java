684f651b6e8b83c2554590d7be37fb714965a686
Ahmad Baraka <ah-baraka@live.com>
2016-04-05 22:08:11 +0200

    Command Input Output refactored to I, R

--- javareactivesprint/src/main/java/io/reactivesprint/rx/Command.java
+++ javareactivesprint/src/main/java/io/reactivesprint/rx/Command.java
@@ -15,21 +15,21 @@ import rx.subjects.Subject;
 
 /**
  * Created by Ahmad Baraka on 5/30/15.
- * Represents a Command or Action that will do some work when executed with {@code Input}
- * and returns {@code Output.}
+ * Represents a Command or Action that will do some work when executed with {@code I}
+ * and returns {@code R.}
  *
  * @see #apply(Object[])
  */
-public final class Command<Input, Output> {
+public final class Command<I, R> {
     //region Fields
 
     private final Scheduler scheduler;
 
-    private final Func1<Input, Observable<Output>> createObservable;
+    private final Func1<I, Observable<R>> createObservable;
 
-    private final Subject<Notification<Output>, Notification<Output>> notifications = PublishSubject.create();
+    private final Subject<Notification<R>, Notification<R>> notifications = PublishSubject.create();
 
-    private final Observable<Output> values;
+    private final Observable<R> values;
     private final Observable<Throwable> errors;
 
     private final MutableProperty<Boolean> executing = new MutableProperty<>(false);
@@ -46,7 +46,7 @@ public final class Command<Input, Output> {
      *
      * @param createObservable Function used to create an Observable each time {@link #apply(Object[])} is invoked.
      */
-    public Command(final Func1<Input, Observable<Output>> createObservable) {
+    public Command(final Func1<I, Observable<R>> createObservable) {
         this(null, new ConstantProperty<>(true), createObservable);
     }
 
@@ -56,7 +56,7 @@ public final class Command<Input, Output> {
      * @param scheduler        Scheduler used to send all notifications in the receiver.
      * @param createObservable Function used to create an Observable each time {@link #apply(Object[])} is invoked.
      */
-    public Command(Scheduler scheduler, final Func1<Input, Observable<Output>> createObservable) {
+    public Command(Scheduler scheduler, final Func1<I, Observable<R>> createObservable) {
         this(scheduler, new ConstantProperty<>(true), createObservable);
     }
 
@@ -67,7 +67,7 @@ public final class Command<Input, Output> {
      * @param createObservable Function used to create an Observable each time {@link #apply(Object[])} is invoked.
      */
     public Command(IProperty<Boolean> enabled,
-                   final Func1<Input, Observable<Output>> createObservable) {
+                   final Func1<I, Observable<R>> createObservable) {
         this(null, enabled, createObservable);
     }
 
@@ -79,32 +79,32 @@ public final class Command<Input, Output> {
      * @param createObservable Function used to create an Observable each time {@link #apply(Object[])} is invoked.
      */
     public Command(Scheduler scheduler, IProperty<Boolean> enabled,
-                   final Func1<Input, Observable<Output>> createObservable) {
+                   final Func1<I, Observable<R>> createObservable) {
         this.scheduler = scheduler;
         this.createObservable = createObservable;
 
-        Observable<Notification<Output>> notificationObservable;
+        Observable<Notification<R>> notificationObservable;
         if (scheduler == null) {
             notificationObservable = notifications;
         } else {
             notificationObservable = notifications.observeOn(scheduler);
         }
 
-        values = notificationObservable.filter(new Func1<Notification<Output>, Boolean>() {
+        values = notificationObservable.filter(new Func1<Notification<R>, Boolean>() {
             @Override
-            public Boolean call(Notification<Output> outputNotification) {
+            public Boolean call(Notification<R> outputNotification) {
                 return outputNotification.isOnNext();
             }
         }).dematerialize();
 
-        errors = notificationObservable.filter(new Func1<Notification<Output>, Boolean>() {
+        errors = notificationObservable.filter(new Func1<Notification<R>, Boolean>() {
             @Override
-            public Boolean call(Notification<Output> outputNotification) {
+            public Boolean call(Notification<R> outputNotification) {
                 return outputNotification.isOnError();
             }
-        }).map(new Func1<Notification<Output>, Throwable>() {
+        }).map(new Func1<Notification<R>, Throwable>() {
             @Override
-            public Throwable call(Notification<Output> outputNotification) {
+            public Throwable call(Notification<R> outputNotification) {
                 return outputNotification.getThrowable();
             }
         });
@@ -131,15 +131,15 @@ public final class Command<Input, Output> {
      * @param inputs Must be either null, empty or contain exactly 1 object.
      */
     @SafeVarargs
-    public final Observable<Output> apply(Input... inputs) {
+    public final Observable<R> apply(I... inputs) {
         if (inputs != null && inputs.length > 1) {
             throw new IllegalStateException("Invalid value of inputs. It must be either null, empty or contain exactly 1 object. Received value is " + Arrays.toString(inputs));
         }
 
-        final Input input = inputs != null && inputs.length > 0 ? inputs[0] : null;
-        return Observable.create(new Observable.OnSubscribe<Output>() {
+        final I input = inputs != null && inputs.length > 0 ? inputs[0] : null;
+        return Observable.create(new Observable.OnSubscribe<R>() {
             @Override
-            public void call(Subscriber<? super Output> subscriber) {
+            public void call(Subscriber<? super R> subscriber) {
                 boolean startedExecuting = false;
 
                 synchronized (lock) {
@@ -154,9 +154,9 @@ public final class Command<Input, Output> {
                     return;
                 }
 
-                Subscription subscription = createObservable.call(input).materialize().subscribe(new Action1<Notification<Output>>() {
+                Subscription subscription = createObservable.call(input).materialize().subscribe(new Action1<Notification<R>>() {
                     @Override
-                    public void call(Notification<Output> outputNotification) {
+                    public void call(Notification<R> outputNotification) {
                         notifications.onNext(outputNotification);
                     }
                 });
@@ -173,26 +173,26 @@ public final class Command<Input, Output> {
     /**
      * An Observable of all {@link Notification} that occur in the receiver.
      */
-    public Observable<Notification<Output>> getNotifications() {
+    public Observable<Notification<R>> getNotifications() {
         return notifications.asObservable();
     }
 
     /**
      * An Observable of Values generated from applications of the receiver.
      */
-    public Observable<Output> getValues() {
+    public Observable<R> getValues() {
         return values;
     }
 
     /**
-     * Whether or not the receiever is enabled.
+     * Whether or not the receiver is enabled.
      */
     public IProperty<Boolean> isEnabled() {
         return enabled;
     }
 
     /**
-     * Whether or not the receiever is executing.
+     * Whether or not the receiver is executing.
      */
     public IProperty<Boolean> isExecuting() {
         if (scheduler == null) {
