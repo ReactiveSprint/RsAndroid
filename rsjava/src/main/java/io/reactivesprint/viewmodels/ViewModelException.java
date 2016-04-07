b02770c5d35f7500119e87c70c68b19ff99def85
Ahmad Baraka <ah-baraka@live.com>
2016-04-07 14:16:48 +0200

    ViewModelException mapErrorFunc tests

--- rsjava/src/main/java/io/reactivesprint/viewmodels/ViewModelException.java
+++ rsjava/src/main/java/io/reactivesprint/viewmodels/ViewModelException.java
@@ -1,10 +1,47 @@
 package io.reactivesprint.viewmodels;
 
+import rx.Observable;
+import rx.Scheduler;
+import rx.functions.Func1;
+
 /**
  * Created by Ahmad Baraka on 4/7/16.
  * Implementation of {@link IViewModelException}.
  */
 public class ViewModelException extends RuntimeException implements IViewModelException {
+    /**
+     * Creates a {@link Func1} which maps sent {@code throwable} to {@link ViewModelException} instance.
+     * <dl>
+     * <dt><b>Example:</b></dt>
+     * <dd>{@code observable.onErrorResumeNext(ViewModelException.<Integer>mapErrorFunc(message));}</dd>
+     * </dl>
+     */
+    public static <T> Func1<Throwable, Observable<T>> mapErrorFunc() {
+        return mapErrorFunc(null);
+    }
+
+    /**
+     * Creates a {@link Func1} which maps sent {@code throwable} to {@link ViewModelException} instance.
+     * <dl>
+     * <dt><b>Example:</b></dt>
+     * <dd>{@code observable.onErrorResumeNext(ViewModelException.<Integer>mapErrorFunc(message));}</dd>
+     * </dl>
+     */
+    public static <T> Func1<Throwable, Observable<T>> mapErrorFunc(final String message) {
+        return new Func1<Throwable, Observable<T>>() {
+            @Override
+            public Observable<T> call(Throwable throwable) {
+                ViewModelException exception;
+                if (message == null) {
+                    exception = new ViewModelException(throwable);
+                } else {
+                    exception = new ViewModelException(message, throwable);
+                }
+                return Observable.error(exception);
+            }
+        };
+    }
+
     public ViewModelException(String message) {
         super(message);
     }
---
+++ rsjava/src/test/java/io/reactivesprint/viewmodels/ViewModelExceptionTest.java
@@ -0,0 +1,28 @@
+package io.reactivesprint.viewmodels;
+
+import junit.framework.TestCase;
+
+import rx.Observable;
+import rx.Subscriber;
+
+import static uk.co.ribot.assertjrx.api.Assertions.*;
+
+/**
+ * Created by Ahmad Baraka on 4/7/16.
+ */
+public class ViewModelExceptionTest extends TestCase {
+
+    static final String EXCEPTION_NAME = "TestException";
+
+    public void testMapErrorFunc() throws Exception {
+
+        Observable<Integer> observable = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                subscriber.onError(new NullPointerException("Test"));
+            }
+        }).onErrorResumeNext(ViewModelException.<Integer>mapErrorFunc(EXCEPTION_NAME));
+
+        assertThat(observable.toBlocking()).failsWithError(ViewModelException.class);
+    }
+}
