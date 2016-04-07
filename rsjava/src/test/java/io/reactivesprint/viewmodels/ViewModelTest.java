b51518a86d35253294917b6101313173ea94708f
Ahmad Baraka <ah-baraka@live.com>
2016-04-07 02:20:16 +0200

    Fixed building issues so travis would work

--- rsjava/src/test/java/io/reactivesprint/viewmodels/ViewModelTest.java
+++ rsjava/src/test/java/io/reactivesprint/viewmodels/ViewModelTest.java
@@ -6,5 +6,7 @@ import junit.framework.TestCase;
  * Created by Ahmad Baraka on 4/7/16.
  */
 public class ViewModelTest extends TestCase {
-
+    public void testTitle() throws Exception {
+        assertTrue(true);
+    }
 }
