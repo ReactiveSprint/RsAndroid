073f71ad5574858664291ad60c8813fe14a8dfff
Ahmad Baraka <ah-baraka@live.com>
2016-05-20 23:23:50 +0200

    Preconditions withMessage methods

--- rsjava/src/main/java/io/reactivesprint/Preconditions.java
+++ rsjava/src/main/java/io/reactivesprint/Preconditions.java
@@ -8,28 +8,40 @@ public final class Preconditions {
     }
 
     public static <T> T checkNotNull(T object, String name) {
+        return checkNotNullWithMessage(object, name + " may not be null.");
+    }
+
+    public static <T> T checkNotNullWithMessage(T object, String message) {
         if (object == null) {
-            throw new NullPointerException(name + " may not be null.");
-        } else {
-            return object;
+            throw new NullPointerException(message);
         }
+
+        return object;
     }
 
     public static <T, L extends Collection<T>> L checkNotNullOrEmpty(L objects, String name) {
-        checkNotNull(objects, name);
+        return checkNotNullOrEmptyWithMessage(objects, name + " may not be empty.");
+    }
+
+    public static <T, L extends Collection<T>> L checkNotNullOrEmptyWithMessage(L objects, String message) {
+        checkNotNullWithMessage(objects, message);
 
         if (objects.isEmpty()) {
-            throw new IllegalStateException(name + " may not be empty.");
+            throw new IllegalStateException(message);
         }
 
         return objects;
     }
 
     public static <T> T[] checkNotNullOrEmpty(T[] objects, String name) {
-        checkNotNull(objects, name);
+        return checkNotNullOrEmptyWithMessage(objects, name + " may not be empty.");
+    }
+
+    public static <T> T[] checkNotNullOrEmptyWithMessage(T[] objects, String message) {
+        checkNotNullWithMessage(objects, message);
 
         if (objects.length == 0) {
-            throw new IllegalStateException(name + " may not be empty.");
+            throw new IllegalStateException(message);
         }
 
         return objects;
