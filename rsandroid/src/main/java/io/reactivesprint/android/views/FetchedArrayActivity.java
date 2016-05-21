625d92eb310c14521c76b5cfd223a6ea9d3f9b19
Ahmad Baraka <ah-baraka@live.com>
2016-05-21 11:18:45 +0200

    Fetched Array Activities

---
+++ rsandroid/src/main/java/io/reactivesprint/android/views/FetchedArrayActivity.java
@@ -0,0 +1,32 @@
+package io.reactivesprint.android.views;
+
+import io.reactivesprint.android.viewmodels.IAndroidViewModel;
+import io.reactivesprint.viewmodels.IFetchedArrayViewModel;
+import io.reactivesprint.views.IFetchedArrayViewController;
+
+/**
+ * Created by Ahmad Baraka on 5/21/16.
+ */
+public abstract class FetchedArrayActivity<VM extends IAndroidViewModel, AVM extends IFetchedArrayViewModel & IAndroidViewModel> extends ArrayActivity<VM, AVM>
+        implements IFetchedArrayViewController<VM, AVM> {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        AVM arrayViewModel = getArrayViewModel();
+        if (arrayViewModel == null) {
+            return;
+        }
+        bindRefreshing(arrayViewModel);
+        bindFetchingNextPage(arrayViewModel);
+    }
+
+    @Override
+    public void bindRefreshing(AVM arrayViewModel) {
+        AndroidViewControllers.bindRefreshing(this, arrayViewModel);
+    }
+
+    @Override
+    public void bindFetchingNextPage(AVM arrayViewModel) {
+        AndroidViewControllers.bindFetchingNextPage(this, arrayViewModel);
+    }
+}
---
+++ rsandroid/src/main/java/io/reactivesprint/android/views/support/FetchedArrayAppCompatActivity.java
@@ -0,0 +1,33 @@
+package io.reactivesprint.android.views.support;
+
+import io.reactivesprint.android.viewmodels.IAndroidViewModel;
+import io.reactivesprint.android.views.AndroidViewControllers;
+import io.reactivesprint.viewmodels.IFetchedArrayViewModel;
+import io.reactivesprint.views.IFetchedArrayViewController;
+
+/**
+ * Created by Ahmad Baraka on 5/21/16.
+ */
+public abstract class FetchedArrayAppCompatActivity<VM extends IAndroidViewModel, AVM extends IFetchedArrayViewModel & IAndroidViewModel> extends ArrayAppCompatActivity<VM, AVM>
+        implements IFetchedArrayViewController<VM, AVM> {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        AVM arrayViewModel = getArrayViewModel();
+        if (arrayViewModel == null) {
+            return;
+        }
+        bindRefreshing(arrayViewModel);
+        bindFetchingNextPage(arrayViewModel);
+    }
+
+    @Override
+    public void bindRefreshing(AVM arrayViewModel) {
+        AndroidViewControllers.bindRefreshing(this, arrayViewModel);
+    }
+
+    @Override
+    public void bindFetchingNextPage(AVM arrayViewModel) {
+        AndroidViewControllers.bindFetchingNextPage(this, arrayViewModel);
+    }
+}
