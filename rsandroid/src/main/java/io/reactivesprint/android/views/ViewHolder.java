3bcb2e5955ffeb370d51b664124b913d7c905d11
Ahmad Baraka <ah-baraka@live.com>
2016-05-21 12:59:58 +0200

    Title TextView for ViewHolder

--- rsandroid/src/main/java/io/reactivesprint/android/views/ViewHolder.java
+++ rsandroid/src/main/java/io/reactivesprint/android/views/ViewHolder.java
@@ -1,10 +1,19 @@
 package io.reactivesprint.android.views;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.TextView;
 
+import com.jakewharton.rxbinding.widget.RxTextView;
+
+import io.reactivesprint.android.R;
 import io.reactivesprint.android.viewmodels.IAndroidViewModel;
+import io.reactivesprint.rx.IMutableProperty;
+import io.reactivesprint.rx.MutableProperty;
 import io.reactivesprint.views.IViewHolder;
+import rx.Observable;
+import rx.functions.Func1;
 
 import static io.reactivesprint.Preconditions.checkNotNull;
 
@@ -14,15 +23,33 @@ public class ViewHolder<VM extends IAndroidViewModel> implements IViewHolder<VM>
     @NonNull
     private final View view;
 
-    private VM viewModel;
+    private TextView titleTextView;
+
+    private final IMutableProperty<VM> viewModel = new MutableProperty<>(null);
 
     //endregion
 
     //region Constructors
 
+    private static TextView createTextView(@NonNull Context context) {
+        checkNotNull(context, "context");
+        TextView textView = new TextView(context);
+        textView.setId(R.id.title);
+        return textView;
+    }
+
+    public ViewHolder(@NonNull Context context) {
+        this(createTextView(context));
+    }
+
     public ViewHolder(@NonNull View view) {
         checkNotNull(view, "view");
         this.view = view;
+        onViewCreated(view);
+    }
+
+    protected void onViewCreated(View view) {
+        titleTextView = (TextView) view.findViewById(R.id.title);
     }
 
     //endregion
@@ -30,16 +57,39 @@ public class ViewHolder<VM extends IAndroidViewModel> implements IViewHolder<VM>
     //region Properties
 
     public void setViewModel(VM viewModel) {
-        this.viewModel = viewModel;
+        this.viewModel.setValue(viewModel);
+
+        if (viewModel == null) {
+            return;
+        }
+
+        if (titleTextView != null) {
+            viewModel.title().getObservable()
+                    .takeUntil(getUntilObservable())
+                    .subscribe(RxTextView.text(titleTextView));
+        }
     }
 
     public VM getViewModel() {
-        return viewModel;
+        return viewModel.getValue();
     }
 
     @NonNull
     public final View getView() {
         return view;
+    }
+
+    public TextView getTitleTextView() {
+        return titleTextView;
+    }
+
+    public Observable<Void> getUntilObservable() {
+        return viewModel.getObservable().map(new Func1<VM, Void>() {
+            @Override
+            public Void call(VM vm) {
+                return null;
+            }
+        });
     }
 
     //endregion
