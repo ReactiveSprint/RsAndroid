c2b2035503cb0e23db592af1f2ce78990045d864
Ahmad Baraka <ah-baraka@live.com>
2016-05-21 11:29:43 +0200

    Passing VM type to IVIew

--- rsjava/src/main/java/io/reactivesprint/views/IViewHolder.java
+++ rsjava/src/main/java/io/reactivesprint/views/IViewHolder.java
@@ -6,7 +6,7 @@ import io.reactivesprint.viewmodels.IViewModel;
  * Created by Ahmad Baraka on 5/21/16.
  * Represents a View in {@code MVVM} that supports setting ViewModel.
  */
-public interface IViewHolder<VM extends IViewModel> extends IView {
+public interface IViewHolder<VM extends IViewModel> extends IView<VM> {
     /**
      * Sets ViewModel to the receiver.
      */
